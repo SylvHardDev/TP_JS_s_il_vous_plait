232da7965142e7799dd902e3e607d83014b934d2
Hardy Sylvestre <hardsyldev@gmail.com>
2023-10-31 07:07:12 +0300

    vitavita style

--- pomodoro/app.js
+++ pomodoro/app.js
@@ -5,6 +5,9 @@ const btnPause = document.querySelector(".b2");
 const btnReset = document.querySelector(".b3");
 const cycles = document.querySelector("h2");
 
+const blocWork = document.querySelector(".travail");
+const blocPause = document.querySelector(".pause");
+
 let checkInterval = false;
 let tempsInitial = 1800;
 let tempsDeRepos = 300;
@@ -23,6 +26,8 @@ btnGo.addEventListener("click", () => {
   if (pause === false && checkInterval === false) {
     checkInterval = true;
 
+    blocWork.classList.add("active");
+
     tempsInitial--;
     affichageTravail.innerText = `${Math.trunc(tempsInitial / 60)} : ${
       tempsInitial % 60 < 10 ? `0${tempsInitial % 60}` : tempsInitial % 60
@@ -30,6 +35,8 @@ btnGo.addEventListener("click", () => {
 
     let timer = setInterval(() => {
       if (pause === false && tempsInitial > 0) {
+        blocWork.classList.add("active");
+        blocPause.classList.remove("active");
         tempsInitial--;
         affichageTravail.innerText = `${Math.trunc(tempsInitial / 60)} : ${
           tempsInitial % 60 < 10 ? `0${tempsInitial % 60}` : tempsInitial % 60
@@ -48,12 +55,17 @@ btnGo.addEventListener("click", () => {
         }`;
       } else if (pause === false && tempsInitial === 0) {
         tempsDeRepos--;
+        blocWork.classList.remove("active");
+        blocPause.classList.add("active");
+
         affichagePause.innerText = `${Math.trunc(tempsDeRepos / 60)} : ${
           tempsDeRepos % 60 < 10 ? `0${tempsDeRepos % 60}` : tempsDeRepos % 60
         }`;
       }
     }, 1000);
     btnReset.addEventListener("click", () => {
+      blocWork.classList.remove("active");
+      blocPause.classList.remove("active");
       clearInterval(timer);
       checkInterval = false;
       tempsInitial = 1800;
@@ -71,12 +83,16 @@ btnGo.addEventListener("click", () => {
 });
 
 btnPause.addEventListener("click", () => {
-  if (pause === false) {
-    btnPause.innerText = "Play";
-    // btnPause.style.backgroundColor = "black"
-  } else if (pause === true) {
-    // btnPause.style.backgroundColor = "white"
-    btnPause.innerText = "Pause";
+  if (checkInterval === true) {
+    if (pause === false) {
+      blocWork.classList.remove("active");
+      btnPause.innerText = "Play";
+      // btnPause.style.backgroundColor = "black"
+    } else if (pause === true) {
+      blocWork.classList.add("active");
+      // btnPause.style.backgroundColor = "white"
+      btnPause.innerText = "Pause";
+    }
+    pause = !pause;
   }
-  pause = !pause;
 });
