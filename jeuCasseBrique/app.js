eea39b1ec79d251ea9451603c47c62bc45251ddc
Hardy Sylvestre <hardsyldev@gmail.com>
2023-11-05 14:01:33 +0300

    fahatelo

--- jeuCasseBrique/app.js
+++ jeuCasseBrique/app.js
@@ -1,5 +1,7 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const affichageScore = document.querySelector(".score");
+
 const rayonBalle = 10,
   BarreHeight = 10,
   barreWidth = 75,
@@ -10,8 +12,10 @@ const rayonBalle = 10,
 
 let x = canvas.width / 2,
   y = canvas.height - 30,
-  barreX = (canvas.width - barreWidth) / 2;
-
+  barreX = (canvas.width - barreWidth) / 2,
+  fin = false,
+  vitesseX = 5,
+  vitesseY = -5;
 function dessineBalle() {
   ctx.beginPath();
   ctx.arc(x, y, rayonBalle, 0, Math.PI * 2);
@@ -66,3 +70,45 @@ function dessineBriques() {
 
 dessineBriques();
 
+function dessine() {
+  if (fin === false) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    dessineBriques();
+    dessineBalle();
+    dessineBarre();
+
+    if (x + vitesseX > canvas.width - rayonBalle || x + vitesseX < rayonBalle) {
+      vitesseX = -vitesseX;
+    }
+
+    if (y + vitesseY < rayonBalle) {
+      vitesseY = -vitesseY;
+    }
+
+    if (y + vitesseY > canvas.height - rayonBalle) {
+      if (x > barreX && x < barreX + barreWidth) {
+        vitesseX = vitesseX + 0.1;
+        vitesseY = vitesseY + 0.1;
+        vitesseY = -vitesseY;
+      } else {
+        fin = true;
+        affichageScore.innerHTML = `Perdu ! <br> Clique sur le casse-brique pour recommencer`;
+      }
+    }
+
+    x += vitesseX;
+    y += vitesseY;
+    requestAnimationFrame(dessine);
+  }
+}
+
+dessine();
+
+//Recomancer
+
+canvas.addEventListener("click", () => {
+  if (fin === true) {
+    fin = false;
+    document.location.reload();
+  }
+});
