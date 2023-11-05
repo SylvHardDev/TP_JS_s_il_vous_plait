08cee9f6cdf3a8110a7c5033bc26616eccd133f5
Hardy Sylvestre <hardsyldev@gmail.com>
2023-11-05 11:56:03 +0300

    creation barre et balle

---
+++ jeuCasseBrique/app.js
@@ -0,0 +1,29 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+const rayonBalle = 10,
+  BarreHeight = 10,
+  barreWidth = 75;
+
+let x = canvas.width / 2,
+  y = canvas.height - 30,
+  barreX = (canvas.width - barreWidth) / 2;
+
+function dessineBalle() {
+  ctx.beginPath();
+  ctx.arc(x, y, rayonBalle, 0, Math.PI * 2);
+  ctx.fillStyle = "#333";
+  ctx.fill();
+  ctx.closePath();
+}
+
+dessineBalle();
+
+function dessineBarre() {
+  ctx.beginPath();
+  ctx.rect(barreX, canvas.height - BarreHeight - 2, barreWidth, BarreHeight);
+  ctx.fillStyle = "#333";
+  ctx.fill();
+  ctx.closePath();
+}
+
+dessineBarre()
